bf3c3041df401583809cefba3e954a586c53ab56
rabhatna <rabhatna@ucsc.edu>
2018-10-17 16:15:07 -0700

    Adopted Lab1 solution, implemented table constraints

--- Lab2/create.sql
+++ Lab2/create.sql
@@ -1,59 +1,61 @@
-/* create.sql is a script to create the schema and tables required for lab 1*/
+-- Using lab 1 solution 
 
-DROP SCHEMA Lab1 CASCADE;
-CREATE SCHEMA Lab1;
+DROP SCHEMA Lab2 CASCADE ;
+CREATE SCHEMA Lab2 ;
+
+CREATE TABLE Authors (
+  authorID          INTEGER 	NOT NULL PRIMARY KEY,
+  authorName        VARCHAR(30),
+  address           VARCHAR(30),
+  numBooksWritten   INTEGER,
+  mostRecentPubDate DATE
+) ;
 
 
-/*Defining tables for Niles Books: Authors, Books, Publishers, Memebers, Orders, and Review */
+CREATE TABLE Books (
+  bookID        CHAR(6) 	NOT NULL PRIMARY KEY,
+  authorID      INTEGER 	NOT NULL,
+  bookName      VARCHAR(30) 	UNIQUE,
+  publisherID   INTEGER 	NOT NULL,
+  pubDate       DATE,
+  price         DECIMAL(6,2),
+  category      CHAR(1),
+  lastOrderDate DATE,
+  totalOrdered  INTEGER
+) ;
 
-CREATE TABLE Authors(
-authorID INTEGER PRIMARY KEY,
-authorName VARCHAR (30),
-address VARCHAR (30),
-numBooksWritten INTEGER,
-mostRecentPubDate DATE
-);
 
-CREATE TABLE Books(
-bookID VARCHAR (6) PRIMARY KEY,
-authorID INTEGER,
-bookName VARCHAR (30),
-publisherID INTEGER,
-pubDate DATE,
-price DECIMAL (6,2),
-category CHAR (1),
-lastOrderDate DATE,
-totalOrdered INTEGER
-);
+CREATE TABLE Publishers (
+  publisherID   INTEGER		NOT NULL PRIMARY KEY,
+  publisherName VARCHAR(30)	UNIQUE,
+  address       VARCHAR(30)
+) ;
 
-CREATE TABLE Publishers(
-publisherID INTEGER PRIMARY KEY,
-publisherName VARCHAR (30), 
-address VARCHAR (30)
-);
 
-CREATE TABLE Members(
-memberID INTEGER PRIMARY KEY,
-memberName VARCHAR (30), 
-joinDate DATE,
-renewalDate DATE,
-isCurrentMember BOOLEAN
-);
+CREATE TABLE Members (
+  memberID        INTEGER 	NOT NULL PRIMARY KEY,
+  memberName      VARCHAR(30)	UNIQUE,
+  joinDate        DATE		UNIQUE,
+  renewalDate     DATE,
+  isCurrentMember BOOLEAN
+) ;
 
-CREATE TABLE Orders(
-memberID INTEGER,
-bookID VARCHAR (6),
-orderDate DATE,
-quantity INTEGER,
-PRIMARY KEY (memberID, bookID, orderDate)
-);
 
-CREATE TABLE Reviews(
-reviewerID INTEGER,
-bookID VARCHAR(6),
-reviewDate DATE,
-reviewStars INTEGER
-);
+CREATE TABLE Orders (
+  memberID  INTEGER 		NOT NULL,
+  bookID    CHAR(6)		NOT NULL,
+  orderDate DATE		NOT NULL,
+  quantity  INTEGER		NOT NULL,
+  PRIMARY KEY (memberID, bookID, orderDate)
+) ;
 
+
+CREATE TABLE Reviews (
+  reviewerID  INTEGER		NOT NULL,
+  bookID      CHAR(6)		NOT NULL,
+  reviewDate  DATE,
+  reviewStars INTEGER		NOT NULL,
+  PRIMARY KEY (reviewerID, bookID)
+) ;
 
 
