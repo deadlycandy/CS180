e5fae28f4d64292bc0f9e295680a82ae683dee5a
rabhatna <rabhatna@ucsc.edu>
2018-10-19 10:20:25 -0700

    Version 1 of query 1

--- Lab2/query1.sql
+++ Lab2/query1.sql
@@ -3,3 +3,8 @@ Find the authorID and authorName for each author whose name doesn't begin
 with the letter S. 
 Your result should appear in descending order of authorName, and no duplicates should appear in your result.
 */
+
+SELECT DISTINCT authorID, authorName
+FROM Authors a
+WHERE a.authorName NOT LIKE "S%" OR a.authorName NOT LIKE "s%"
+ORDER BY authorName DESC;
