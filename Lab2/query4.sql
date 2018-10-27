94ebf53700cbe52c068bae7b5fee2989cb7f4e31
rabhatna <rabhatna@ucsc.edu>
2018-10-26 18:51:02 -0700

    Query4 up to lab specification

--- Lab2/query4.sql
+++ Lab2/query4.sql
@@ -5,3 +5,11 @@ Find the reviewerID and bookID for each review that has all of the followingthre
 • The reviewer is a member whose name is 'John Smith'.
 No duplicates should appear in your result.
 */
+
+
+SELECT DISTINCT reviewerID AS "Reviewer ID", b.bookID AS "Book ID"
+FROM Reviews AS r, Books AS b
+WHERE r.reviewStars < 4 and b.bookName = 'Jane Eyre' and r.reviewerID IN 
+    (SELECT memberID 
+     FROM Members AS m 
+     WHERE m.memberName = 'John Smith');
