616178bcd0cfa6f2664e60ce2dd0c471278ccc21
rabhatna <rabhatna@ucsc.edu>
2018-11-09 12:35:38 -0800

    foreign.sql is up to specification

--- Lab3/foreign.sql
+++ Lab3/foreign.sql
@@ -1,10 +1,10 @@
 /* Adding foreign constraints to tables */
 
 ALTER TABLE Books
-ADD CONSTRAINT constrain_authorID FOREIGN KEY (authorID) REFERENCES Authors(authorID);
+ADD CONSTRAINT fk_authorID FOREIGN KEY (authorID) REFERENCES Authors(authorID);
 
 ALTER TABLE Orders
-ADD CONSTRAINT constrain_bookID FOREIGN KEY (bookID) REFERENCES Books(bookID);
+ADD CONSTRAINT fk_bookID FOREIGN KEY (bookID) REFERENCES Books(bookID);
 
 ALTER TABLE Reviews
-ADD CONSTRAINT constrain_reviewerID FOREIGN KEY (reviewerID) REFERENCES Members(memberID);
+ADD CONSTRAINT fk_reviewerID FOREIGN KEY (reviewerID) REFERENCES Members(memberID);
