6851ba0e8ba43d3abf2723dac3137a83325cbfae
rabhatna <rabhatna@ucsc.edu>
2018-11-18 14:29:55 -0800

    Turned combine into a serializable transaction

--- Lab3/combine.sql
+++ Lab3/combine.sql
@@ -1,15 +1,19 @@
 /* Combining tuples from NewMemberships into Memebers */
 
 /* updating for existing members*/
+BEGIN;
 UPDATE Members
 SET memberName = newM.memberName, renewalDate = newM.renewalDate, isCurrentMember = TRUE
 FROM NewMemberships AS newM
 WHERE Members.memberID = newM.memberID;
+COMMIT;
 
 /*inserting new members*/
+BEGIN;
 INSERT INTO Members(memberID, memberName, joinDate, renewalDate, isCurrentMember)
 SELECT memberID, memberName, CURRENT_DATE, renewalDate, TRUE
 FROM NewMemberships AS newM
 WHERE memberID NOT IN 
                 (SELECT memberID
                  FROM Members);
+COMMIT;
